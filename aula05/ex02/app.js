3c19e34ce0a510c8ba556ddd377d361d3f161454
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-04 10:22:13 -0300

    Exercício Tabuada Finalizado

--- aula05/ex02/app.js
+++ aula05/ex02/app.js
@@ -4,13 +4,13 @@ AUTOR: ARTHUR PILOTO
 DATA DE CRIAÇÃO: 04/08/2022
 VERSÃO: 1.0
 ******************************************************************/
-console.log('\nCALCULAR TABUADA SIMPLES')
+console.log(`\nCALCULAR TABUADA SIMPLES`)
 
 // Import da função calcularTabuada
 // Import e criação da instância de interação com usuário
 const { calcularTabuada } = require(`./modulos/tabuada.js`)
-var readline = require('readline')
-const { exit } = require('process')
+var readline = require(`readline`)
+const { exit } = require(`process`)
 var entradaDados = readline.createInterface ({
     input: process.stdin,
     output: process.stdout
