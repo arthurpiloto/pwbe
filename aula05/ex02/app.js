ee1c5bc42e1a1a406e536f6c72c94b2434aeb314
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-04 09:02:50 -0300

    estrutura do novo exercício

---
+++ aula05/ex02/app.js
@@ -0,0 +1,14 @@
+/******************************************************************
+OBJETIVO: APLICAR ESTRUTURAS DE REPETIÇÃO (WHILE E FOR)
+AUTOR: ARTHUR PILOTO
+DATA DE CRIAÇÃO: 04/08/2022
+VERSÃO: 1.0
+******************************************************************/
+console.log('\nCALCULAR TABUADA SIMPLES')
+
+var readline = require('readline')
+var entradaDados = readline.createInterface ({
+    input: process.stdin,
+    output: process.stdout
+})
+
---
+++ aula05/ex02/modulos/tabuada.js
@@ -0,0 +1,11 @@
+/******************************************************************
+OBJETIVO: CRIAÇÃO DE FUNÇÕES PARA CALCULAR UMA TABUADA
+AUTOR: ARTHUR PILOTO
+DATA DE CRIAÇÃO: 04/08/2022
+VERSÃO: 1.0
+******************************************************************/
+
+const calcularTabuada = function (numero1) {
+    let tabuada = numero1
+    let resultado
+}
