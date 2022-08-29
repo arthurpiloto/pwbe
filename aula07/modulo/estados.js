ad353567ad50dd8fe1cd97e80b05a80dbd7b68c0
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-29 08:38:59 -0300

    novo exercício

---
+++ aula07/modulo/estados.js
@@ -0,0 +1,7 @@
+/******************************************************************
+OBJETIVO: OBTER UMA LISTA DE ESTADOS. EXERCÍCIO FEITO JUNTO COM 
+O PROFESSOR, USO DE ARRAY E JSON
+AUTOR: ARTHUR PILOTO
+DATA DE CRIAÇÃO: 29/08/2022
+VERSÃO: 1.0
+******************************************************************/
