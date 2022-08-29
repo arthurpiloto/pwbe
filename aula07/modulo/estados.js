aa1e36ec9197bbb64419b4ed9845c5a7f524b6d7
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-29 09:02:20 -0300

    trabalhando com array, json e forEach

--- aula07/modulo/estados.js
+++ aula07/modulo/estados.js
@@ -5,3 +5,52 @@ AUTOR: ARTHUR PILOTO
 DATA DE CRIAÇÃO: 29/08/2022
 VERSÃO: 1.0
 ******************************************************************/
+
+// Simulando o resultado de uma API
+var estados = [
+    {
+        sigla: `SP`,
+        nome: `São Paulo`
+    },
+    {
+        sigla: `AC`,
+        nome: `Acre`
+    },
+    {
+        sigla: `RJ`,
+        nome: `Rio de Janeiro`
+    },
+    {
+        sigla: `BA`,
+        nome: `Bahia`
+    },
+    {
+        sigla: `CE`,
+        nome: `Ceará`
+    },
+    {
+        sigla: `MG`,
+        nome: `Minas Gerais`
+    },
+    {
+        sigla: `GO`,
+        nome: `Goiás`
+    },
+]
+
+// Retorna todos os estados pela sigla
+const getSiglaEstados = function () {
+    let listaEstados = []
+    let erro = true
+
+    estados.forEach(item => {
+        listaEstados.push(item.sigla)
+        erro = false
+    })
+    
+    if (erro) {
+        return false
+    } else {
+        return listaEstados
+    }
+}
