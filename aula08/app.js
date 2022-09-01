28e59cf83f3e49ee5e9306f9bcba835ae35b5bd5
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-09-01 11:17:12 -0300

    import of the libraries

--- aula08/app.js
+++ aula08/app.js
@@ -9,3 +9,11 @@ BIBLIOTECAS NECESSÁRIAS PARA CRIAR UMA API
 2º - Cors -> npm install cors --save; É uma biblioteca para manipular as permissões do protocolo http
 3º - Body-Parser -> npm install body-parser --save; É uma biblioteca que permite manipular o corpo do protocolo http
 */
+
+// Import da biblioteca do Express para criar a API
+const express = require(`express`)
+// Import da biblioteca do Cors para manipular as permissões do protocolo http
+const cors = require(`cors`)
+// Import da biblioteca do Body-Parser para manipular o corpo das requisições do protocolo http
+const bodyParser = require(`body-parser`)
+
