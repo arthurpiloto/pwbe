f1d4554f8712cb27104ab06bc4063209b449f681
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-09-01 11:07:25 -0300

    class 08

---
+++ aula08/app.js
@@ -0,0 +1,11 @@
+/******************************************************************
+OBJETIVO: AULA SOBRE CRIAÇÃO E CONSUMO DE APIs
+AUTOR: ARTHUR PILOTO
+DATA DE CRIAÇÃO: 01/09/2022
+******************************************************************/
+/*
+BIBLIOTECAS NECESSÁRIAS PARA CRIAR UMA API
+1º - Express -> npm install express --save; É uma biblioteca para criar aplicações em node no formato de API
+2º - Cors -> npm install cors --save; É uma biblioteca para manipular as permissões do protocolo http
+3º - Body-Parser -> npm install body-parser --save; É uma biblioteca que permite manipular o corpo do protocolo http
+*/
