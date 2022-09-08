89e020d6016a053f340fcee15ef121afff8dc6ab
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-09-08 09:26:39 -0300

    new class exercise

---
+++ aula09/app.js
@@ -0,0 +1,8 @@
+/******************************************************************
+OBJETIVO: EXERCÍCIO DE CRIAÇÃO DE UMA API
+AUTOR: ARTHUR PILOTO
+DATA DE CRIAÇÃO: 08/09/2022
+******************************************************************/
+const express = require(`express`)
+const cors = require(`cors`)
+const bodyParser = require(`body-parser`)
