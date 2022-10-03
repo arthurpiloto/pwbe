080d258fc88baf2cffbb7de74b06f191c094b0c7
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-10-03 11:00:03 -0300

    express, cors e body parser instalados

---
+++ aula010/app.js
@@ -0,0 +1,9 @@
+/******************************************************************
+OBJETIVO: EXERCÍCIO DE INTEGRAÇÃO DE BANCO DE DADOS COM BACK-END
+AUTOR: ARTHUR PILOTO
+DATA DE CRIAÇÃO: 03/10/2022
+******************************************************************/
+const express = require(`express`)
+const app = express()
+const cors = require(`cors`)
+const bodyParser = require(`body-parser`)
