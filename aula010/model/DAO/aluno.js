9e4fc420d4a62f231d7a69936fe31146fb016e91
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-10-06 10:07:10 -0300

    criando a estrutura CRUD

---
+++ aula010/model/DAO/aluno.js
@@ -0,0 +1,27 @@
+/************************************************************************
+OBJETIVO: ARQUIVO RESPONSÁVEL PELA MANIPULAÇÃO DE DADOS 
+COM O BD (insert, update, delete e select)
+AUTOR: ARTHUR PILOTO
+DATA DE CRIAÇÃO: 06/10/2022
+VERSÃO: 1.0
+************************************************************************/
+
+// Função para inserir um novo registro no BD
+const insertAluno = async (aluno) => {
+
+}
+
+// Função para atualizar um registro no BD
+const updateAluno = async (aluno) => {
+
+}
+
+// Função para excluir um registro no BD
+const deleteAluno = async (id) => {
+
+}
+
+// Função para retornar todos os registros do BD
+const selectAllAluno = async (aluno) => {
+
+}
