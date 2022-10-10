f2dc87b1e425aaab4c90d4fc1f8940c993d494a3
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-10-10 08:11:56 -0300

    estrutura padrão da API construída

--- aula010/app.js
+++ aula010/app.js
@@ -1,18 +1,27 @@
-/******************************************************************
-OBJETIVO: EXERCÍCIO DE INTEGRAÇÃO DE BANCO DE DADOS COM BACK-END
+/************************************************************************
+OBJETIVO: API RESPONSÁVEL PELA MANIPULAÇÃO DE DADOS DO BACK-END.
+(GET, POST, PUT, DELETE)
 AUTOR: ARTHUR PILOTO
-DATA DE CRIAÇÃO: 03/10/2022
-******************************************************************/
-const express = require(`express`)
-const app = express()
-const cors = require(`cors`)
-const bodyParser = require(`body-parser`)
+DATA DE CRIAÇÃO: 10/10/2022
+VERSÃO: 1.0
 
-/* 
+ANOTAÇÕES:
 Para manipular o acesso a BD podemos usar o Prisma. Para instalá-lo, devemos rodar os seguintes comandos:
 NOVOS COMANDOS RODADOS
-npm install prisma --save
-npx prisma
-npx prisma init
-npm install @prisma/client
-*/
+- npm install prisma --save
+- npx prisma
+- npx prisma init
+- npm install @prisma/client
+************************************************************************/
+
+const express = require(`express`)
+const cors = require(`cors`)
+const bodyParser = require(`body-parser`)
+const app = express()
+
+app.use((request, response, next) => {
+    response.header(`Access-Control-Allow-Origin`, `*`)
+    response.header(`Access-Control-Allow-Methods`, `GET, POST, PUT, DELETE, OPTIONS`)
+    app.use(cors())
+    next()
+})
